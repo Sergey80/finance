fef5397bb6e65c2b7af35471d28f20b88da6fabd
Sergey Starodubtsev <ses.box@gmail.com>
2016-03-05 13:59:06 -0500

    OptionOrderTicket

--- src/main/scala/model/trading/securities/derivatives/options/ticket/OptionOrderTicket.scala
+++ src/main/scala/model/trading/securities/derivatives/options/ticket/OptionOrderTicket.scala
@@ -1,9 +1,7 @@
 package model.trading.securities.derivatives.options.ticket
 
-import model.trading.account.AccountType._
 import model.trading.account.{AccountType, Account}
 import model.trading.securities.derivatives.options.OptionType
-import model.trading.securities.derivatives.options.ticket.TransactionType.TransactionType
 import model.trading.{Price, Date}
 import model.trading.orders.OrderSide
 import model.trading.securities.Security
@@ -18,27 +16,47 @@ object TransactionType extends Enumeration {
   // Solicited orders: in which an advisor recommends a transaction to a client, provides additional information about a transaction to a client necessary for the client to make an informed investment decision, or promotes a security by describing its merits in a positive fashion, and the client follows the advisor’s recommendation.
 }
 
+/*
+ * A form detailing an order instruction that a customer gives an account executive.
+ * --
+ * Prior to transmitting a customer’s option order,
+ * the representative must fill out the appropriate order ticket and present it
+ * to the trading department or wire room for execution.
+ */
+case class OptionOrderTicket(
 
-class OptionOrderTicket {
-
-  val side          : OrderSide.Value   = OrderSide.Buy
+  side          : OrderSide.Value    = OrderSide.Buy,
   // underlying security can be any asset, index, financial instrument or even another derivative
-  val underlying    : Security          = new Stock("Google")
-  val quantity      : Int               = 1 // "Number of contracts (1 contract = 100 securities)"
-  val optType       : OptionType.Value  = OptionType.CALL
-  val expiration    : Date              = "17 Jun 2016"
-  val exercisePrice : Price             = 100.00
-  val indication    : String            = "if transaction is opening or closing the contracts"
-  val covered                           = "If an opening sale, a notation regarding covered or uncovered is required"
-  val account       : Account           = Account(name = "ABC", number = "101-131315-434")
-  val accountType   : AccountType.Value = AccountType.Cash
-  val price                             = "Price and time limits if any"
-  val transactionType                   = TransactionType.Solicited
-  val d                                 = "Discretionary authority exercised or discretionary authority not exercised, if applicable"
+  underlyingName : String            =  "Google", //new Stock("Google"),
+  quantity       : Long              = 1, // "Number of contracts (1 contract = 100 securities)"
+  optType        : OptionType.Value  = OptionType.CALL,
+  expiration     : Date              = "17 Jun 2016",
+  exercisePrice  : Price             = 100.00,
 
-  val timeEntered   : String            = ""
-  val timeExecuted  : String            = ""
-  val timeChanged   : String            = ""
-  val timeCanceled  : String            = ""
+  // TODO:
+  indication     : String            = "if transaction is opening or closing the contracts",
+  covered        : String            = "If an opening sale, a notation regarding covered or uncovered is required",
+
+  account        : Account           = Account(name = "ABC", number = "101-131315-434"),
+  accountType    : AccountType.Value = AccountType.Cash,
+
+  // TODO:
+  price          : String            = "Price and time limits if any",
+
+  transactionType: TransactionType.Value  = TransactionType.Solicited,
+
+  // TODO:
+  d              : String            = "Discretionary authority exercised or discretionary authority not exercised, if applicable",
+
+  // TODO:
+  timeEntered    : String            = "",
+  timeExecuted   : String            = "",
+  timeChanged    : String            = "",
+  timeCanceled   : String            = ""
+
+)
+
+
+object OptionOrderTicket {
 
 }
---
+++ src/test/scala/model/trading/EndToEndOption.scala
@@ -0,0 +1,8 @@
+package model.trading
+
+object EndToEndOption extends App {
+
+  // 2. Order Ticket  (instructing to execute)
+  // 3. Executing order -> Order (executed)
+
+}
